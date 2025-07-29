e2c05f9cdf3d6f6d9fe0b2ac3762ad2c7a09c234
Abdulsalam <abdulkareemabdulsalam.iotbtech@hotmail.com>
2025-07-29 02:04:25 +0100

    popularArtists fix

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SideBar from "./sideBar";
 import Hero from "./Hero";
-import PopularArtists from "./PopularArtists";
+import PopularArtists from "./popularArtists";
 import Footer from "./Footer";
 
 export default class Dashboard extends React.Component {
